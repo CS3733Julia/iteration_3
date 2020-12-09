7b9812b09b8422bf8dfa321ffc797f7b279004b2
nastallings <46728228+nastallings@users.noreply.github.com>
2020-12-09 10:32:42 -0500

    Nate's branch
    * completed the complete choice which is completed with a choice which
    once completed has been completely completed
    
    * updated lambda functions for approve and disapprove
    
    * Eclipse is being weird. this commit does not work but I am pushing this
    to ensure I don't lose any progress. Will fix in next push
    
    * Updated ParticipateInChoiceLambda with 3 new features:
    
    1) The function now checks to make sure the password is correct rather
    then creating a new member with the same name but this new member aslo
    has a password. It returns a wrong password error.
    
    2) Function now returns a too many members error
    
    3) Function now returns a choice does not exist error
    
    * deleted Manfest.mf
    
    * Added function to check if choice hase been completed
    
    * Made request fail if the choice has already been created for approve and
    disapprove
    
    * Updated front end code- this push includes selecting an alternative and
    adding feedback

--- js/participateInChoice.js
+++ js/participateInChoice.js
@@ -2,43 +2,63 @@
  * participating in a choice
  */
 function processParticipant(result, member) {
-	console.log(result)
-	console.log(member);
-	document.getElementById( "display" ).style.display = "block"
-	document.getElementById("display_choiceDescription").innerHTML = result["description"];
-	document.getElementById("date_of_creation").innerHTML = "Date of Creation: " + result["dateCreate"];
-    document.getElementById("unique_id").innerHTML = "Choice ID: " + result["idChoice"];
-    document.getElementById("member_id").innerHTML = member;
-	if(result["dateComplete"] != "") {
-		console.log(result["dateComplete"])
-		document.getElementById("date_of_completion").innerHTML = "Date of Completion: " + result["dateComplete"];
+	if(member == "N/A"){
+		document.getElementById( "Error" ).style.display = "block"
+		document.getElementById("Error").innerHTML = result;
 	}
-	alt_array = result["alternatives"]
-    console.log(alt_array)
-    for(var i = 0; i < 5; i++){ 
-        document.getElementById("alt" + (i + 1) + "_card").style.display = 'none';     
-    }
-
-	for (var i = 0; i < alt_array.length; i++) {
-		processPreference(result, alt_array[i].idAlternative)
-		console.log(alt_array[i].descriptionAlternative);
-		console.log("a" + (i + 1) + "_description")
-		document.getElementById("a" + (i + 1) + "_descriptiondisplay").innerHTML = alt_array[i].descriptionAlternative;
-		document.getElementById("a" + (i + 1) + "_id").innerHTML = alt_array[i].idAlternative;
-		document.getElementById("alt" + (i + 1) + "_card").style.display = 'block'; 
+	else{	
+		document.getElementById( "Error" ).style.display = "none"
+		document.getElementById("unique_id").innerHTML = "";
+		console.log(result)
+		console.log(member);
+		document.getElementById( "display" ).style.display = "block"
+		document.getElementById("display_choiceDescription").innerHTML = result["description"];
+		document.getElementById("date_of_creation").innerHTML = "Date of Creation: " + result["dateCreate"];
+		document.getElementById("unique_id").innerHTML = "Choice ID: " + result["idChoice"];
+		document.getElementById("raw_choice_id").innerHTML = result["idChoice"];
+		document.getElementById("member_id").innerHTML = member;
 		if(result["dateComplete"] != "") {
-			if(alt_array[i].isChosen){
-				document.getElementById("a" + (i + 1) + "_header").style = "background-color: green";
-				document.getElementById("a" + (i + 1) + "_descriptiondisplay").innerHTML += "- Chosen"
+			console.log(result["dateComplete"])
+			document.getElementById("date_of_completion").innerHTML = "Date of Completion: " + result["dateComplete"];
+		}
+
+		
+		alt_array = result["alternatives"]
+		console.log(alt_array)
+		for(var i = 0; i < 5; i++){ 
+			document.getElementById("alt" + (i + 1) + "_card").style.display = 'none';     
+		}
+		processFeedback(result, "N/A")
+		for (var i = 0; i < alt_array.length; i++) {
+			processPreference(result, alt_array[i].idAlternative)
+			console.log(alt_array[i].descriptionAlternative);
+			console.log("a" + (i + 1) + "_description")
+			document.getElementById("a" + (i + 1) + "_descriptiondisplay").innerHTML = alt_array[i].descriptionAlternative;
+			document.getElementById("a" + (i + 1) + "_id").innerHTML = alt_array[i].idAlternative;
+			document.getElementById("alt" + (i + 1) + "_card").style.display = 'block'; 
+			if(result["dateComplete"] != "") {
+				console.log("Jackichan")
+
+				if(alt_array[i].isChosen){
+					document.getElementById("a" + (i + 1) + "_header").style = "background-color: green";
+					document.getElementById("a" + (i + 1) + "_descriptiondisplay").innerHTML += "- Chosen"
+				}
+				document.getElementById("a" + (i + 1) + "_approveButton").disabled = true;
+				document.getElementById("a" + (i + 1) + "_disapproveButton").disabled = true;
+				document.getElementById("a" + (i + 1) + "_select_button").disabled = true;
+				document.getElementById("a" + (i + 1) + "_addFeedbackButton").disabled = true;
+				document.getElementById("a" + (i + 1) + "_feedbackDescription").disabled = true;
 			}
-			document.getElementById("a" + (i + 1) + "_approveButton").disabled = true;
-			document.getElementById("a" + (i + 1) + "_disapproveButton").disabled = true;
-			document.getElementById("a" + (i + 1) + "_select_button").disabled = true;
-			document.getElementById("a" + (i + 1) + "_addFeedbackButton").disabled = true;
-			document.getElementById("a" + (i + 1) + "_feedbackDescription").disabled = true;
-		}  
-	}
-	
+			else{
+				document.getElementById("a" + (i + 1) + "_header").style = "background-color: light grey";
+				document.getElementById("a" + (i + 1) + "_approveButton").disabled = false;
+				document.getElementById("a" + (i + 1) + "_disapproveButton").disabled = false;
+				document.getElementById("a" + (i + 1) + "_select_button").disabled = false;
+				document.getElementById("a" + (i + 1) + "_addFeedbackButton").disabled = false;
+				document.getElementById("a" + (i + 1) + "_feedbackDescription").disabled = false;
+			}
+		}
+	}	
 }
 
 function handleParticipantClick(e) {
@@ -67,12 +87,19 @@ function handleParticipantClick(e) {
 		if (xhr1.readyState == XMLHttpRequest.DONE) {
 			if (xhr1.status == 200) {
 				var js = JSON.parse(xhr1.responseText);
-				processParticipant(js["choice"], js["idMember"]);
+				console.log(js["statusCode"]);
+				if(js["statusCode"] == 400){
+					processParticipant(js["error"], "N/A");
+				}
+				else{
+					processParticipant(js["choice"], js["idMember"]);
+				}
 			} else {
 				console.log("actual:" + xhr1.responseText)
 				var js = JSON.parse(xhr1.responseText);
+				console.log(js);
 				var err = js["response"];
-				alert(err);
+				processParticipant(err, "N/A");
 			}
 		} else {
 			processParticipant("N/A", "N/A");
