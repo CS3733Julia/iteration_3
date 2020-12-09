619cbf600d659f20e91118f11db2d1f72f0b0b9e
elizabethkirschner <43473208+elizabethkirschner@users.noreply.github.com>
2020-12-08 22:30:12 -0500

    Liz's branch
    * add feedback lambda
    
    * Added function to check if choice hase been completed
    
    * fix lambda to refuse if choice is complete, added tests

--- src/main/java/com/julia/model/Feedback.java
+++ src/main/java/com/julia/model/Feedback.java
@@ -1,11 +1,31 @@
 package com.julia.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Feedback {
-	public final String description;
+	public final String idChoice;
+	public final String idAlternative;
 	public final String idMember;
+	public final String description;
+	public final String date;	
 	
-	public Feedback(String description, String idMember) {
-		this.description = description;
+	public Feedback(String idChoice, String idAlternative, String idMember, String description) {
+		this.idChoice = idChoice;
+		this.idAlternative = idAlternative;
 		this.idMember = idMember;
+		this.description = description;
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime date = LocalDateTime.now();
+		this.date = date.format(formatter);
+	}
+	
+	public Feedback(String idMember, String description, String date) {
+		this.idChoice = null;
+		this.idAlternative = null;
+		this.idMember = idMember;
+		this.description = description;
+		this.date = date;
 	}
 }
